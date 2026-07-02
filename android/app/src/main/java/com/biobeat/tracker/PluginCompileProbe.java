c86ae5aeb261e4bd7905366b2071d3d3e6ceca86
charly7978 <Carlos_ameghino@hotmail.com>
2026-07-01 22:48:49 -0300

    Add Capacitor plugin compile probe

---
+++ android/app/src/main/java/com/biobeat/tracker/PluginCompileProbe.java
@@ -0,0 +1,8 @@
+package com.biobeat.tracker;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "PluginCompileProbe")
+public class PluginCompileProbe extends Plugin {
+}
