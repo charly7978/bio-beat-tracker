7aad56047508d22cdcaaa8f0f79ea3d68a770615
charly7978 <Carlos_ameghino@hotmail.com>
2026-07-01 22:50:47 -0300

    Use Camera2 capability reader in native plugin

--- android/app/src/main/java/com/biobeat/tracker/NativePpgCapturePlugin.java
+++ android/app/src/main/java/com/biobeat/tracker/NativePpgCapturePlugin.java
@@ -1,6 +1,5 @@
 package com.biobeat.tracker;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
@@ -10,10 +9,6 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 public class NativePpgCapturePlugin extends Plugin {
     @PluginMethod
     public void getCapabilities(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("available", false);
-        ret.put("provider", "camera2");
-        ret.put("reason", "capability_scaffold");
-        call.resolve(ret);
+        call.resolve(Camera2CapabilityReader.read(getContext()));
     }
 }
