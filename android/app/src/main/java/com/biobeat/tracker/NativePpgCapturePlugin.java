f04d9f214b211bbd75a342480fdab4e7e2f7e765
charly7978 <Carlos_ameghino@hotmail.com>
2026-07-01 22:44:21 -0300

    Mark native PPG plugin as capability scaffold

--- android/app/src/main/java/com/biobeat/tracker/NativePpgCapturePlugin.java
+++ android/app/src/main/java/com/biobeat/tracker/NativePpgCapturePlugin.java
@@ -1,4 +1,8 @@
 package com.biobeat.tracker;
 
+/**
+ * Native PPG capture scaffold.
+ * Next step: replace this shell with a registered Capacitor plugin backed by Camera2/CameraX.
+ */
 public class NativePpgCapturePlugin {
 }
